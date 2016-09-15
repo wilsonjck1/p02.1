9b2e6a4a506e9c4039d448b62da317f5321731f8
Andrew Charlton <a.charlton32@gmail.com>
2016-09-15 20:48:37 +0100

    Added fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,34 @@
+"""
+Problem:
+
+    FizzBuzz is a counting game. Players take turns counting the next number
+    in the sequence 1, 2, 3, 4 ...  However, if the number is:
+
+    * A multiple of 3 -> Say 'Fizz' instead
+    * A multiple of 5 -> Say 'Buzz' instead
+    * A multiple of 3 and 5 -> Say 'FizzBuzz' instead
+    
+    The function fizzbuzz should take a number and print out what the player
+    should say.
+
+Tests:
+
+    >>> fizzbuzz(7)
+    7
+    >>> fizzbuzz(10)
+    Buzz
+    >>> fizzbuzz(12)
+    Fizz
+    >>> fizzbuzz(30)
+    FizzBuzz
+
+"""
+
+# Use this to test your solution. Don't edit it!
+import doctest
+def run_tests():
+    doctest.testmod(verbose=True)
+
+
+# Edit this function
+def fizzbuzz(n):
