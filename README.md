7db7198c3d2e1af97bc27e2d197b03a47b3f61cd
Andrew Charlton <a.charlton32@gmail.com>
2016-09-15 21:18:57 +0100

    Hit_100 added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Problems
 
-- [ ] 
+- [ ] hit_100 
 - [ ]
 - [ ] roll_double
 - [ ]
---
+++ hit_100.py
@@ -0,0 +1,28 @@
+"""
+Problem:
+
+     A game is played by trying to score exactly 100 points.
+     The function hit_100 takes in a number of points. If the score is
+     100, it should print "Winner!". For scores over 100, it should
+     print "Too high", and for scores under 100 it should print "Too low"
+
+Tests:
+
+    >>> hit_100(72)
+    Too low
+    >>> hit_100(135)
+    Too high
+    >>> hit_100(100)
+    Winner!
+
+"""
+
+# Use this to test your solution. Don't edit it!
+import doctest
+def run_tests():
+    doctest.testmod(verbose=True)
+
+
+# Edit this code
+def hit_100(score):
+
